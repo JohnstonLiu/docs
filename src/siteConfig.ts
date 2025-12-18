138c54965bb1ac03768235a56fa2227f97e96384
Johnston Liu <johnstonliu2004@gmail.com>
2025-12-18 01:09:10 -0800

    init: astro port

---
+++ src/siteConfig.ts
@@ -0,0 +1,28 @@
+import type { SiteConfiguration, SocialLinks } from "@/types.ts";
+
+export const SITE: SiteConfiguration = {
+  title: "johnston's docs",
+  description: "Technical docs by Johnston Liu",
+  href: "https://johnstonliu.me",
+  author: "Johnston Liu",
+  locale: "en-US",
+};
+
+export const SOCIAL_LINKS: SocialLinks = {
+  personal: {
+    label: "johnstonliu.me",
+    href: "https://johnstonliu.me",
+  },
+  email: {
+    label: "email",
+    href: "mailto:johnstonliu2004@gmail.com",
+  },
+  github: {
+    label: "github",
+    href: "https://github.com/johnstonliu",
+  },
+  linkedin: {
+    label: "linkedin",
+    href: "https://linkedin.com/in/johnstonliu",
+  },
+};
